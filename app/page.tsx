d94858514a71f6ea598ebf7e6ac7fe9364b68e06
patrykstk <code.patrykstasik@gmail.com>
2025-04-07 00:51:01 +0200

    feat: add basic landing page and update middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { Logo } from "@/components/logo";
 
 const HomePage = () => {
-  return <div>Tu kiedyś coś będzie</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen space-y-10 bg-yellow-500">
+      <div className="flex flex-col space-y-5 items-center">
+        <h6 className="text-6xl font-bold uppercase font-sans">
+          Zacznij korzystać z aplikacji
+        </h6>
+        <div className="flex flex-row gap-x-4">
+          <Image
+            src={"./python_logo.svg"}
+            alt={"Logo aplikacji"}
+            width={100}
+            height={400}
+          />
+          <Logo />
+        </div>
+      </div>
+
+      <div className="flex flex-row gap-x-5">
+        <Link href="/register">
+          <Button size="lg" className="text-lg px-8">
+            REJESTRACJA
+          </Button>
+        </Link>
+        <Link href="/login">
+          <Button size="lg" className="text-lg px-8">
+            LOGOWANIE
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,35 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const sessionToken = req.cookies.get("sessionToken")?.value;
+  const { pathname } = req.nextUrl;
 
-  if (!sessionToken) {
+  if (
+    sessionToken &&
+    (pathname === "/login" || pathname === "/register" || pathname === "/")
+  ) {
+    return NextResponse.redirect(new URL("/home", req.url));
+  }
+
+  if (
+    !sessionToken &&
+    pathname !== "/login" &&
+    pathname !== "/register" &&
+    pathname !== "/"
+  ) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/home", "/manage", "/settings/:path*", "/tasks/:path*"],
+  matcher: [
+    "/",
+    "/home",
+    "/manage",
+    "/settings/:path*",
+    "/tasks/:path*",
+    "/login",
+    "/register",
+  ],
 };
